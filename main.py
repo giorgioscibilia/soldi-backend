bbf4c4d1025d95782688aa796821c011d326e15a
Giorgio Scibilia <giorgioscibilia@gmail.com>
2026-02-09 17:00:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
-import functions_framework
+import os
 import gspread
 from google.auth import default
+from flask import Flask, request
 
-@functions_framework.http
-def process_payslips(request):
-    # ID del tuo nuovo file di test
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def run_task():
     sheet_id = "1GUAQwmmjqi40Ni9x5m9pjEbCXfGf1HAslnhJPgOoWD4"
-    
     try:
-        # Usa le credenziali del Service Account di GCP
         creds, _ = default()
         gc = gspread.authorize(creds)
         sh = gc.open_by_key(sheet_id)
         sheet = sh.worksheet("Log_Compensation")
         
-        sheet.append_row(["09/02/2026", "1.00", "GITHUB_TEST", "System", "Deploy da GitHub OK"])
-        return "Connessione GitHub-Sheets riuscita!", 200
+        sheet.append_row(["09/02/2026", "1.00", "GITHUB_RUN_OK", "System", "Cloud Run is alive!"])
+        return "Connessione stabilita!", 200
     except Exception as e:
         return f"Errore: {str(e)}", 500
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
